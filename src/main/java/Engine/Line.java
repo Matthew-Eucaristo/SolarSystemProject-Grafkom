481120f2ab65107fe785d64a3e0c35a8f49a9fc0
SyNTaX-PC <108221736+SyNTaX-TheTrickster@users.noreply.github.com>
2023-04-17 01:30:24 +0700

    changes in Line Constructor for ease of use

--- src/main/java/Engine/Line.java
+++ src/main/java/Engine/Line.java
@@ -1,10 +1,25 @@
 package Engine;
 
+import org.joml.Vector3f;
+
+import java.util.Collections;
+import java.util.List;
+
 import static org.lwjgl.opengl.GL11C.*;
 
 public class Line extends Object{
     public Line(float red, float green, float blue, float alpha) {
         super(red, green, blue, alpha);
+    }
+
+    public Line(float[] rgba) {
+        this(rgba[0], rgba[1], rgba[2], rgba[3]);
+    }
+
+    public Line(float[] rgba, List<Vector3f> vertices) {
+        this(rgba);
+        this.vertices = Collections.singletonList((Vector3f) vertices);
+        setupVAOVBO();
     }
 
     @Override
